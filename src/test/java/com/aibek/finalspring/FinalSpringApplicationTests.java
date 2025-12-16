8200a85ea021c21a06f39e0b099a7029c80edf2b
AIBEK <aibek.kemel@narxoz.kz>
2025-12-16 22:36:47 +0500

    initial commit

---
+++ src/test/java/com/aibek/finalspring/FinalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aibek.finalspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
